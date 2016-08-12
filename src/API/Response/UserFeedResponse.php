fbfbfdfb8987641719e2829ca3e64d227b63225c
Liam Cottle <liam@liamcottle.com>
2016-08-12 15:04:32 +1200

    Add next_max_id to UserFeedResponse Fixes #17

--- src/API/Response/UserFeedResponse.php
+++ src/API/Response/UserFeedResponse.php
@@ -23,6 +23,12 @@ class UserFeedResponse extends BaseResponse {
     protected $more_available;
 
     /**
+     * Next Maximum Id
+     * @var string
+     */
+    protected $next_max_id;
+
+    /**
      * @return int
      */
     public function getNumResults()
@@ -68,6 +74,22 @@ class UserFeedResponse extends BaseResponse {
     public function setMoreAvailable($more_available)
     {
         $this->more_available = $more_available;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNextMaxId()
+    {
+        return $this->next_max_id;
+    }
+
+    /**
+     * @param string $next_max_id
+     */
+    public function setNextMaxId($next_max_id)
+    {
+        $this->next_max_id = $next_max_id;
     }
 
 }
